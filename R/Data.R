92ecf96724224e3f417c74cc85b1262e73e6688a
ctross <ctross@ucdavis.edu>
2019-01-12 23:08:39 +0100

    Create Data.R

---
+++ R/Data.R
@@ -0,0 +1,29 @@
+#' SkewCalc: Estimate reproductive skew
+#'
+#' Documentatiion here is bare-bones, but will be updated as needed.
+#'
+#' @name SkewCalc
+#' @docType package
+NULL
+
+#' @title KipsigisMales
+#' @description Reproductive success and age for a census of Kipsigis men.
+#' @format A data frame with 848 rows and 3 variables:
+#' \describe{
+#'   \item{\code{code}}{Personal ID code.}
+#'   \item{\code{age}}{Age at death or last census.}
+#'   \item{\code{rs}}{Reproductive success.}
+#'}
+#' @source Monique Borgerhoff Mulder
+"KipsigisMales"
+
+#' @title SukumaMales
+#' @description Reproductive success and age for a census of Sukuma men.
+#' @format A data frame with 63 rows and 3 variables:
+#' \describe{
+#'   \item{\code{code}}{Personal ID code.}
+#'   \item{\code{age}}{Age at death or last census.}
+#'   \item{\code{rs}}{Reproductive success.}
+#'}
+#' @source Monique Borgerhoff Mulder
+"SukumaMales"
