2d4e822bc37b7bdf4b49bb681a50daaf3948b042
ctross <ctross@ucdavis.edu>
2019-07-12 13:26:28 +0200

    Update M_Index_from_B_Index.R

--- R/M_Index_from_B_Index.R
+++ R/M_Index_from_B_Index.R
@@ -2,7 +2,9 @@
 #'
 #' @param B The B index value estimated from a dataset.
 #' @param R Total number of RS events in the sample from which B was estimated.
-#' @param R Total number of individuals in the sample from which B was estimated.
+#' @param N Total number of individuals in the sample from which B was estimated.
+#' @param t Assumption of the exposure time vector used in the correction term.
+#' @param Samples Samples used in estimating the correction term.
 #' @return The M index.
 #' @examples
 #' B = 0.1 
@@ -10,7 +12,13 @@
 #' N = 10
 #' M_index_from_B_index(B, R, N)
 
-M_index_from_B_index = function(B, R, N) {
-  M = sqrt(B*R - 1/N + 1)
+M_index_from_B_index = function(B, R, N, t=rep(1/N,N),Samples=1000) {
+      E_Mraw_sq = rep(NA,Samples)
+    for(j in 1: Samples){
+      t_hat = t/sum(t)
+      E_Mraw_sq[j] <- Mraw_index(rmultinom(1,R,t_hat),t)^2
+      }
+ M = Mraw_index_from_B_index(B,R,N)^2 - mean(E_Mraw_sq)
   return(M)
 }
+
