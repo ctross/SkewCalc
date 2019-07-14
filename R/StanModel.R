1e5118407b1aa19d491cd7dfb62b051d4eb72133
ctross <ctross@ucdavis.edu>
2019-07-14 15:34:30 +0200

    Update StanModel.R

--- R/StanModel.R
+++ R/StanModel.R
@@ -53,7 +53,7 @@ model{
  T_star = sum(pow2(t,gamma) - pow2(t0,gamma));
  t_hat_star = (pow2(t,gamma) - pow2(t0,gamma))/T_star;
  
- alpha ~ dirichlet(rep_vector(1,N));
+ alpha ~ dirichlet(t_hat);
  
  r ~ multinomial(t_hat); 
  r ~ multinomial(t_hat_star); 
