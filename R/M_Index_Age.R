5f98a07a6b7a5c5020ba48625da968e3710aae70
ctross <ctross@ucdavis.edu>
2019-07-17 08:44:15 +0200

    Update M_Index_Age.R

--- R/M_Index_Age.R
+++ R/M_Index_Age.R
@@ -13,13 +13,13 @@ M_index_age = function(r,t,t0=0,Samples=1000) {
   if(min(t-t0) <= 0){
   return(NA)
    } else{
-     E_Mraw_sq = rep(NA,Samples)
+     E_Mraw = rep(NA,Samples)
     for(j in 1: Samples){
       R = sum(r)
       t_hat = t/sum(t)
-      E_Mraw_sq[j] <- Mraw_index_age(rmultinom(1,R,t_hat),t,t0)^2
+      E_Mraw[j] <- Mraw_index_age(rmultinom(1,R,t_hat),t,t0)
       }
-    M = Mraw_index_age(r,t,t0)^2 - mean(E_Mraw_sq)
+    M = Mraw_index_age(r,t,t0) - mean(E_Mraw)
   return(M)
   }
 }
