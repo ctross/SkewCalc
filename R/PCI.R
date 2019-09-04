10f9aa7a1272afa60937eb7dd546a7f159836d20
ctross <ctross@ucdavis.edu>
2019-09-04 14:45:07 +0200

    Update PCI.R

--- R/PCI.R
+++ R/PCI.R
@@ -33,3 +33,26 @@ concat <- function (...)
 {
     paste(..., collapse = "", sep = "")
 }
+
+HPDI <- function (samples, prob = 0.89) 
+{
+    class.samples <- class(samples)[1]
+    coerce.list <- c("numeric", "matrix", "data.frame", "integer", 
+        "array")
+    if (class.samples %in% coerce.list) {
+        samples <- coda::as.mcmc(samples)
+    }
+    x <- sapply(prob, function(p) coda::HPDinterval(samples, 
+        prob = p))
+    n <- length(prob)
+    result <- rep(0, n * 2)
+    for (i in 1:n) {
+        low_idx <- n + 1 - i
+        up_idx <- n + i
+        result[low_idx] <- x[1, i]
+        result[up_idx] <- x[2, i]
+        names(result)[low_idx] <- concat("|", prob[i])
+        names(result)[up_idx] <- concat(prob[i], "|")
+    }
+    return(result)
+}
