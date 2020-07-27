2aefea285253dca5fa09e8ebbfa2c999c61722ad
ctross <ctross@ucdavis.edu>
2020-07-27 15:59:25 +0200

    Create StanModel_GRF.R

---
+++ R/StanModel_GRF.R
@@ -0,0 +1,120 @@
+model_code <-'
+functions{
+ real Mraw(vector r, vector t){
+   int N = rows(r);
+
+   real R; 
+   real T; 
+   vector[N] s; 
+   real Mraw;                       
+
+   R = sum(r);
+   T = sum(t);  
+
+   for(i in 1:N)
+   s[i] = ((r[i]/R)-(t[i]/T))^2;  
+ 
+   Mraw = N * sum(s); 
+   return Mraw;            
+  }
+
+ vector pow2(vector x, real y){
+   vector[rows(x)] z;
+   for (i in 1:rows(x)) 
+   z[i] = x[i]^y;
+   return(z);
+  }
+}
+
+data{
+ int N;
+ int A;
+ int r[N];
+ vector[N] t; 
+ vector[N] t0;  
+}
+
+transformed data{
+ int R;
+ 
+ R = sum(r);
+}
+
+parameters{
+ simplex[N] alpha;
+ real<lower=0> gamma;
+ real Concentration;
+ vector[A] theta;
+}
+
+model{ 
+ real T;
+ real T_star;
+ 
+ vector[N] t_eff;
+ vector[N] t_hat;
+ vector[N] t_hat_star;
+ 
+ gamma ~ normal(0,1);
+ Concentration ~ normal(0,2.5);
+ 
+ for(i in 1:N){
+  t_hat_star[i] = 0;
+   for(a in (t0[i]+1):t[i]){
+    t_eff[i] += theta[a];
+    }}
+ 
+ T = sum(t-t0);
+ t_hat = (t-t0)/T;
+ 
+ T_star = sum(t_eff);
+ t_hat_star = (t_eff)/T_star;
+ 
+ alpha ~ dirichlet(t_hat*exp(Concentration));
+ 
+ r ~ multinomial(t_hat); 
+ r ~ multinomial(t_hat_star); 
+ r ~ multinomial(alpha); 
+}
+
+generated quantities{
+ real M_raw;
+ real M;
+ real M_raw_age;
+ real M_age;
+ 
+  { 
+    real Bias;
+    real T;
+    real T_star;
+ 
+    vector[N] t_eff;
+    vector[N] t_hat;
+    vector[N] t_hat_star;
+
+    for(i in 1:N){
+     t_hat_star[i] = 0;
+     for(a in (t0[i]+1):t[i]){
+     t_eff[i] += theta[a];
+      }}
+ 
+    T = sum(t-t0);
+    t_hat = (t-t0)/T;
+ 
+    T_star = sum(t_eff);
+    t_hat_star = (t_eff)/T_star;
+
+    M_raw =  Mraw(to_vector(multinomial_rng(alpha,R)),t_hat);
+    M_raw_age =  Mraw(to_vector(multinomial_rng(alpha,R)),t_hat_star);
+ 
+    Bias = Mraw(to_vector(multinomial_rng(t_hat,R)),t_hat);
+    M = M_raw - Bias;
+ 
+    Bias = Mraw(to_vector(multinomial_rng(t_hat_star,R)),t_hat_star);
+    M_age = M_raw_age - Bias;
+ }
+} 
+
+'
+
+#
