245968990d37987dd0e33d9ebcd4de0f766dc0db
ctross <ctross@ucdavis.edu>
2022-07-05 12:01:23 +0200

    Update StanModel.R

--- R/StanModel.R
+++ R/StanModel.R
@@ -60,8 +60,8 @@ model{
  
  T_star = sum(pow2(t,gamma) - pow2(t0,gamma));
  t_hat_star = (pow2(t,gamma) - pow2(t0,gamma))/T_star;
- //t_hat_star*exp(Concentration)
- alpha ~ dirichlet((pow2(t,gamma) - pow2(t0,gamma)));
+ 
+ alpha ~ dirichlet(t_hat_star*exp(Concentration));
  
  //r ~ multinomial(t_hat); 
  //r ~ multinomial(t_hat_star); 
